ee39022b5187f435406314113acb36da35e606f3
sssulai@gmail.com <you@example.com>
2021-09-27 19:39:38 -0400

    added content to applications view

--- theme/src/views/sections/Applications.vue
+++ theme/src/views/sections/Applications.vue
@@ -1,6 +1,6 @@
 <template>
   <base-section id="applications">
-    <base-section-heading title="Applications">
+    <base-section-heading title="Applications Built">
       </base-section-heading>
 
     <v-container>
@@ -15,12 +15,12 @@
             horizontal
           >
           <v-card-title>Project 1. </v-card-title>
-            <p><a href="partydash.com">Party Dash App</a></p>
-           <p>We built Party Dash with a simple goal in mind, to help party hosts plan parties more easily.</p>
-            <p>Followed design specs handed down from site owner.</p> 
-            <p>Used CSS and HTML to create inviting website</p>
-            <p>Technologies used: HTML, CSS, Bootstrap </p>
-            <p><a href="https://partydash.com/pages/about-us" target="_blank">Read More</a></p>
+            <p class="font-weight-black"><a href="partydash.com">Party Dash App</a></p>
+            <p class="font-weight-black">We built Party Dash with a simple goal in mind, to help party hosts plan parties more easily.</p>
+            <p class="font-weight-black">Followed design specs handed down from site owner.</p> 
+            <p class="font-weight-black">Used CSS and HTML to create inviting website</p>
+            <p class="font-weight-black">Technologies used: HTML, CSS, Bootstrap </p>
+            <p class="font-weight-black"><a href="https://partydash.com/pages/about-us" target="_blank">Read More</a></p>
             </base-avatar-card>
         </v-col>
       </v-row>
@@ -35,16 +35,16 @@
             horizontal
           >
             <v-card-title>Project 2.</v-card-title>
-            <p>RuneSkype</p>
-            <p>RuneSkype is our final project for the Columbia Coding Bootcamp. It is the newest and best way for you and your friends to interact while playing your favorite Role Playing Game (RPG). Getting Started Playing RuneSkype is easy, just download a copy from our homepage or our repo:</p>
-            <p>Responsibilities:</p> 
-            <p>Getting Started:</p>
-            <p>RuneSkype is the newest and best way for you and your friends to interact while playing your favorite Role Playing Game (RPG). In our case that is RuneSkype.
+            <p class="font-weight-black">RuneSkype</p>
+            <p class="font-weight-black">RuneSkype is our final project for the Columbia Coding Bootcamp. It is the newest and best way for you and your friends to interact while playing your favorite Role Playing Game (RPG). Getting Started Playing RuneSkype is easy, just download a copy from our homepage or our repo:</p>
+            <p class="font-weight-black">Responsibilities:</p> 
+            <p class="font-weight-black">Getting Started:</p>
+            <p class="font-weight-black">RuneSkype is the newest and best way for you and your friends to interact while playing your favorite Role Playing Game (RPG). In our case that is RuneSkype.
               Playing RuneSkype is easy, just download a copy from our homepage or our repo: https://runeskype.herokuapp.com/play https://github.com/Jroze88/RuneSkype</p>
-            <p>From there you'l want to intstall React, Node, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios, and React-Drag. These dependencies will allow you to set up the RuneSkype environment. These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.</p>
-            <p>Prerequisites and Dependencies: </p>
-            <p>Reactjs, Nodejs, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios, React-Drag, material-ui</p>
-            </base-avatar-card>
+            <p class="font-weight-black">From there you'l want to intstall React, Node, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios, and React-Drag. These dependencies will allow you to set up the RuneSkype environment. These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.</p>
+            <p class="font-weight-black">Prerequisites and Dependencies: </p>
+            <p class="font-weight-black">Reactjs, Nodejs, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios, React-Drag, material-ui</p>
+          </base-avatar-card>
         </v-col>
       </v-row>
       <v-row>
@@ -58,18 +58,39 @@
             horizontal
           >
             <v-card-title>Project 3.</v-card-title>
-            <p>The G.O.A.T. Voter App</p>
-            <p><a href="http://www.goatvoter.org" target="_blank">http://www.goatvoter.org/</a></p>
-            <p>Responsibilities:</p> 
-            <p>Front-End Design, created the CSS color scheme and front-end components.</p>
-            <p>The GOAT Voter App is a one-stop platform where voters can access information about candidates running for political offices anywhere in the United States. The GOAT Voter
-               App streamlines information and increases voter participation. repo: By entering their zip code in a search field, users will be able to search and access detailed information about all candidates running for election in their district. In addition, users can enter their address to find out where their polling site is located and request transportation on election day.</p>
-            <p>For a full description of the GOAT Voter App, please visit the project page:
-               www.goatvoter.org | <a href="https://github.com/naz585/Project-2">https://github.com/naz585/Project-2</a> </p>
-            <p>Prerequisites and Dependencies: </p>
-            <p>JavaScript, Handlebars, Bootstrap, CSS Parallax, Node.js, Express, Sequelize, Google Civic Information API, Heroku,
-            </p>
-            </base-avatar-card>
+            <p class="font-weight-black"><a href="www.goatvoter.org">G.O.A.T. Voter App</a> </p>
+            <p class="font-weight-black">The GOAT Voter App is a one-stop platform where voters can access information about candidates running for political offices anywhere in the United States. The GOAT Voter App streamlines information and increases voter participation.</p>
+            <p class="font-weight-black">By entering their zip code in a search field, users will be able to search and access detailed information about all candidates running for election in their district. In addition, users can enter their address to find out where their nearest polling site is located and request transportation on election day.</p>
+            <p class="font-weight-black">Responsibilities:</p> 
+            <p class="font-weight-black">Front-End Design, created the color scheme and front-end components.</p>
+            <p class="font-weight-black">By entering their zip code in a search field, users will be able to search and access detailed information about all candidates running for election in their district. In addition, users can enter their address to find out where their nearest polling site is located and request transportation on election day.</p>
+            <p class="font-weight-black">Technologies used:</p>
+            <p class="font-weight-black">JavaScript, Handlebars, Bootstrap, CSS Parallax, Node.js, Express, Sequelize, Google Civic Information API, Heroku </p>
+            <p class="font-weight-black"></p>
+          </base-avatar-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <base-avatar-card
+            v-bind="feature"
+            align="left"
+            horizontal
+          >
+            <v-card-title>Project 4.</v-card-title>
+            <p class="font-weight-black"><a href="www.https://sulai3030.github.io/test-BeerNearHere/g">Beer Near Here</a> </p>
+            <p class="font-weight-black">Beer Near Here</p>
+            <p class="font-weight-black">By entering their zip code in a search field, users will be able to search and access detailed information about all candidates running for election in their district. In addition, users can enter their address to find out where their nearest polling site is located and request transportation on election day.</p>
+            <p class="font-weight-black">Responsibilities:</p> 
+            <p class="font-weight-black">Backend Design</p>
+            <p class="font-weight-black">Beer Near Here is an app that services beer drinkers in and around the New York and New Jersey areas using user generated information via questionnaire about beer preferences returning locations that serve craft beers.</p>
+            <p class="font-weight-black">Technologies used:</p>
+            <p class="font-weight-black">HTML5, CSS, and JavaScript, Express, Bootstrap, GitHub Pages</p>
+            <p class="font-weight-black"></p>
+          </base-avatar-card>
         </v-col>
       </v-row>
     </v-container>
@@ -90,3 +111,6 @@
     }),
   }
 </script>
+<style scoped>
+
+</style>
--- theme/src/views/sections/Clients.vue
+++ theme/src/views/sections/Clients.vue
@@ -1,116 +1,119 @@
 <template>
+  <v-theme-provider dark>
+    <v-img
+      :min-height="minHeight"
+      :src="require('@/assets/light.jpg')"
+      class="white--text"
+      gradient="to bottom, rgba(11, 28, 61, .9), rgba(0,0,0,.4)"
+    >
   <base-section id="clients">
-    <div :style="{ backgroundColor: backgroundcolor }" id="wrapper">
-    <v-container>
-      <base-section-heading title="Clients">
-      </base-section-heading>
-      <v-row>
-        <v-col
-          cols="1"
-          style="max-width: 100%;"
-          class="flex-grow-1 flex-shrink-0"
-        >
-          <base-avatar-card
-            v-bind="feature"
-            align="left"
-            horizontal
-          >
-              <v-list-item>
-                <v-card-title>BlackHealth<br />Matters.com </v-card-title>
-                  <v-list-item-content>
-                    <v-list-item-title>
-                     <p><a href="blackhealthmatters.com" target="_blank">BlackHealthMatters.com</a></p>
-                      <p><a href="https://blackhealthmatters.com/lauren-lee-talks-kidney-disease-breakthroughs/" target="_blank">Lauren Lee talks kidney disease breakthroughs</a></p>
-                      <p><a href="https://blackhealthmatters.com/is-there-a-light-at-the-end-of-the-dark-tunnel-of-kidney-disease-in-children/" target="_blank">Is There a Light at the End of the Dark Tunnel of Kidney Disease in Children?" - An Interview with Dr. Gbadegesin</a></p>
-                      <p><a href="https://blackhealthmatters.com/working-with-your-doctor-when-you-have-kidney-disease-empowering-yourself-to-be-an-advocate-for-you" target="_blank">Working with your doctor when you have kidney disease empowering yourself to be an advocate for you/</a></p>  
-                      <p><a href="https://blackhealthmatters.com/kidney-disease/what-is-fsgs/" target="_blank"> What is FSGS?</a></p>
-                      <p><a href="The Truth About Chronic Kidney Disease" target="_blank">The Truth About Chronic Kidney Disease</a></p>
-                      <p><a href="How Can I Be In A Clinical Trial?" target="_blank">How Can I Be In A Clinical Trial?</a></p>
-                    </v-list-item-title>
-                  </v-list-item-content>
-              </v-list-item>
-              <v-list-item>
-                <v-card-title>PartyDash</v-card-title>
-                  <v-list-item-content>
-                    <v-list-item-title>
-                     <p><a href="https://partydash.com/" target="_blank"></a></p>
-                     <p>Front End Design</p>
-                     <p>We built Party Dash with a simple goal in mind: to help party hosts plan parties more easily</p>
-                     <p>Responsibilities: Follow design specs handed down from site owner. Used CSS and HTML to create inviting website</p>
-                     <p>Technologies used: HTML, CSS, Bootstrap</p>
-                    </v-list-item-title>
-                  </v-list-item-content>
-              </v-list-item>
-              <v-list-item>
-                <v-card-title>Mayapple Center<br /> for the Arts and<br /> Humanities </v-card-title>
-                  <v-list-item-content>
-                    <v-list-item-title>
-                     <p><a href="mayapplecenter.org" target="_blank"></a></p>
-                      <p>Updated and maintained website (Drupal platform), creating links between Mayapple’s homepage and their yearly summer program at Sarah Lawrence College. Added, removed, and refined content for Events, Faculty, Executive Staff, Executive Board, and Advisory Board pages.      	
-                      Key Accomplishments:</p>
-                      <p> Updated homepage to include crucial information about and links to Mayapple’s program at Sarah Lawrence College</p>
-                      <p> Removed departed Advisory Board members from Advisory Board page and added new Advisory Board Members </p>  
-                      <p> Input new faculty on Faculty page and Executive Board listing and names.</p>
-                      <p>	Communicated foundation’s message through updates on homepage about mission, events, enrollment opportunities and scholarships</p>
-                    </v-list-item-title>
-                  </v-list-item-content>
-              </v-list-item>
-          </base-avatar-card>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <base-avatar-card
-            v-bind="feature"
-            align="left"
-            horizontal
-          >
-            <v-card-title>Project 2.</v-card-title>
-            <p>RuneSkype</p>
-            <p>RuneSkype is our final project for the Columbia Coding Bootcamp. It is the newest and best way for you and your friends to interact while playing your favorite Role Playing Game (RPG). Getting Started Playing RuneSkype is easy, just download a copy from our homepage or our repo:</p>
-            <p>Responsibilities:</p> 
-            <p>Getting Started:</p>
-            <p>RuneSkype is the newest and best way for you and your friends to interact while playing your favorite Role Playing Game (RPG). In our case that is RuneSkype.
-              Playing RuneSkype is easy, just download a copy from our homepage or our repo: https://runeskype.herokuapp.com/play https://github.com/Jroze88/RuneSkype</p>
-            <p>From there you'l want to intstall React, Node, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios, and React-Drag. These dependencies will allow you to set up the RuneSkype environment. These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.</p>
-            <p>Prerequisites and Dependencies: </p>
-            <p>Reactjs, Nodejs, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios, React-Drag, material-ui</p>
-            </base-avatar-card>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <base-avatar-card
-            v-bind="feature"
-            align="left"
-            horizontal
-          >
-            <v-card-title>Project 3.</v-card-title>
-            <p>The G.O.A.T. Voter App</p>
-            <p><a href="http://www.goatvoter.org" target="_blank">http://www.goatvoter.org/</a></p>
-            <p>Responsibilities:</p> 
-            <p>Front-End Design, created the CSS color scheme and front-end components.</p>
-            <p>The GOAT Voter App is a one-stop platform where voters can access information about candidates running for political offices anywhere in the United States. The GOAT Voter
-               App streamlines information and increases voter participation. repo: By entering their zip code in a search field, users will be able to search and access detailed information about all candidates running for election in their district. In addition, users can enter their address to find out where their polling site is located and request transportation on election day.</p>
-            <p>For a full description of the GOAT Voter App, please visit the project page:
-               www.goatvoter.org | <a href="https://github.com/naz585/Project-2">https://github.com/naz585/Project-2</a> </p>
-            <p>Prerequisites and Dependencies: </p>
-            <p>JavaScript, Handlebars, Bootstrap, CSS Parallax, Node.js, Express, Sequelize, Google Civic Information API, Heroku,
-            </p>
-            </base-avatar-card>
-        </v-col>
-      </v-row>
-    </v-container>
-    </div>
-  </base-section>
+  <v-container>
+    <v-row no-gutters>
+      <v-col>
+        <div :style="{ backgroundColor: backgroundcolor }" id="wrapper">
+          <base-section-heading title="Clients"></base-section-heading>
+          <v-row>
+            <v-col
+              md="12"
+            >
+                  <v-card-title>BlackHealthMatters.com </v-card-title>
+                  <v-list-item>
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          <v-card-text>
+                              <p><a href="blackhealthmatters.com" target="_blank">BlackHealthMatters.com</a></p>
+                              <p class="text--primary"><a href="https://blackhealthmatters.com/lauren-lee-talks-kidney-disease-breakthroughs/" target="_blank">Lauren Lee talks kidne disease breakthroughs</a></p>
+                              <p class="text--primary"><a href="https://blackhealthmatters.com/is-there-a-light-at-the-end-of-the-dark-tunnel-of-kidney-disease-in-children/" target="_blank">Is There a Light at the End of the Dark Tunnel of Kidney Disease in Children?" - An Interview with Dr. Gbadegesin</a></p>
+                              <p class="text--primary"><a href="https://blackhealthmatters.com/working-with-your-doctor-when-you-have-kidney-disease-empowering-yourself-to-be-an-advocate-for-you"     target="_blank">Working with your doctor when you have kidney disease empowering yourself to be an advocate for you/</a></p>  
+                              <p class="text--primary"><a href="https://blackhealthmatters.com/kidney-disease/what-is-fsgs/" target="_blank"> What is FSGS?</a></p>
+                              <p class="text--primary"><a href="The Truth About Chronic Kidney Disease" target="_blank">The Truth About Chronic Kidney Disease</a></p>
+                              <p class="text--primary"><a href="How Can I Be In A Clinical Trial?" target="_blank">How Can I Be In A Clinical Trial?</a></p>
+                          </v-card-text>
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-card-title>PartyDash</v-card-title>
+                    <v-list-item>  
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            <v-card-text>
+                              <p><a href="https://partydash.com/" target="_blank"></a></p>
+                              <p>Front End Design</p>
+                              <p>We built Party Dash with a simple goal in mind: to help party hosts plan parties more easily</p>
+                              <p>Responsibilities: Follow design specs handed down from site owner. Used CSS and HTML to create inviting website</p>
+                              <p>Technologies used: HTML, CSS, Bootstrap</p>
+                            </v-card-text>
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    <v-card-title>Mayapple Center for the Arts and Humanities </v-card-title>
+                    <v-list-item>  
+                       <v-list-item-content>
+                          <v-list-item-title>
+                            <v-card-text>
+                                <p><a href="mayapplecenter.org" target="_blank"></a></p>
+                                <p>Updated and maintained website (Drupal platform), creating links between Mayapple’s homepage and their yearly summer program at Sarah Lawrence    College.    Added, removed, and refined content for Events, Faculty, Executive Staff, Executive Board, and Advisory Board pages.      	
+                                Key Accomplishments:</p>
+                                <p> Updated homepage to include crucial information about and links to Mayapple’s program at Sarah Lawrence College</p>
+                                <p> Removed departed Advisory Board members from Advisory Board page and added new Advisory Board Members </p>  
+                                <p> Input new faculty on Faculty page and Executive Board listing and names.</p>
+                                <p>	Communicated foundation’s message through updates on homepage about mission, events, enrollment opportunities and scholarships</p>
+                            </v-card-text>
+                          </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+          </v-row>
+          <v-row>
+            <v-col
+              md="6"
+            >
+              <base-avatar-card
+                v-bind="feature"
+                align="left"
+                horizontal
+              >
+                <v-card-title>Project 2.</v-card-title>
+                <p>RuneSkype</p>
+                <p>RuneSkype is our final project for the Columbia Coding Bootcamp. It is the newest and best way for you and your friends to interact while playing your favorite Role Playing Game (RPG). Getting Started Playing RuneSkype is easy, just download a copy from our homepage or our repo:</p>
+                <p>Responsibilities:</p> 
+                <p>Getting Started:</p>
+                <p>RuneSkype is the newest and best way for you and your friends to interact while playing your favorite Role Playing Game (RPG). In our case that is RuneSkype.
+                  Playing RuneSkype is easy, just download a copy from our homepage or our repo: https://runeskype.herokuapp.com/play https://github.com/Jroze88/RuneSkype</p>
+                <p>From there you'l want to intstall React, Node, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios, and React-Drag. These dependencies will allow you to set up the RuneSkype environment. These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.</p>
+                <p>Prerequisites and Dependencies: </p>
+                <p>Reactjs, Nodejs, Express, Firebase, Twilio-Video, Twilio, WebRTC, Axios, React-Drag, material-ui</p>
+                </base-avatar-card>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col
+              
+              md="6"
+            >
+              <base-avatar-card
+                v-bind="feature"
+                align="left"
+                horizontal
+              >
+                <v-card-title>Project 3.</v-card-title>
+                <p>The G.O.A.T. Voter App</p>
+                <p><a href="http://www.goatvoter.org" target="_blank">http://www.goatvoter.org/</a></p>
+                <p>Responsibilities:</p> 
+                <p>Front-End Design, created the CSS color scheme and front-end components.</p>
+                <p>The GOAT Voter App is a one-stop platform where voters can access information about candidates running for political offices anywhere in the United States. The GOAT Voter
+                  App streamlines information and increases voter participation. repo: By entering their zip code in a search field, users will be able to search and access detailed information about all candidates running for election in their district. In addition, users can enter their address to find out where their polling site is located and request transportation on election day.</p>
+                <p>For a full description of the GOAT Voter App, please visit the project page:
+                  www.goatvoter.org | <a href="https://github.com/naz585/Project-2">https://github.com/naz585/Project-2</a> </p>
+                <p>Prerequisites and Dependencies: </p>
+                <p>JavaScript, Handlebars, Bootstrap, CSS Parallax, Node.js, Express, Sequelize, Google Civic Information API, Heroku,
+                </p>
+                </base-avatar-card>    
+            </v-col>
+        </v-row>
+  </v-container> 
+  </v-theme-provider>
 </template>
-
+        
 <script>
   export default {
     name: 'SectionThemeFeatures',
@@ -123,15 +126,10 @@
         },
       ],
     }),
-      myStyle: {
-        backgroundColor: "#6495F5"
-      }
   }
 </script>
 <style scoped>
-  #wrapper {
-    background-color: #6495F5;    
-    width: 100%;
-    color: #fff;
+  a{
+    color: goldenrod;
   }
 </style>
